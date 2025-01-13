c4a8692b32d0098020878dd9d66436a1dcf3e11f
nadea287 <n_nadea@inbox.ru>
2025-01-13 23:22:15 +0200

    created `user` table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { User } from "./auth/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '1',
+      database: 'nest_jwt_test',
+      port: 3306,
+      charset: 'utf8mb4_general_ci',
+      entities: [
+        User
+      ],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
